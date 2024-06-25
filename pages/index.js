e049069cf7dca1cbefc28cf765dc332b83af3d40
kite <kite@kites-MacBook-Pro.local>
2024-06-25 10:04:51 +0530

    add files

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,47 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import InfluencerCard from '../components/InfluencerCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../styles/globals.css';
 
 export default function Home() {
+  const influencers = ["Rachana Ranade", "Akshat Shrivastav", "Pranjal Kamra", "Sunil Mingalani", "Ravi Kumar Stock market"];
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Money control | Daily Updates of Stock market</title>
       </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="container-fluid bg-dark text-white">
+        <div className="container">
+          <div className="row align-items-center py-4">
+            <div className="col-md-6">
+              <h2>Daily Updates of Stock market</h2>
+            </div>
+            <div className="col-md-6">
+              <img src="../public/IMG.png" alt="Stock Market" />
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      </div>
+      <div className="container my-4">
+        <h2>Influencers :</h2>
+        <div className="container-wrapper row my-4">
+          {influencers.map(name => (
+            <InfluencerCard key={name} name={name} />
+          ))}
+          <div className="stock-info"></div>
+        </div>
+        <h3 className="generate-text my-2">Results:</h3>
+        <div className="card col-md-12 my-4">
+          <div className="my-3">
+            <div className="title-text">
+              <h5 className="bottom-text">Expected Stocks:</h5>
+              <br />
+              <div id="chatgpt-response"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
